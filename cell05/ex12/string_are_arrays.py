8037810bf8508903d728af7a3d1ef18d7c51411d
jaikwang <kanna9764@gmail.com>
2025-09-17 06:47:18 +0700

    c5,c6

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+args = sys.argv[1:]
+
+if len(args) != 1:
+    print("none")
+else:
+    s = args[0]
+    found = False
+    for ch in s:
+        if ch == "z":
+            print("z", end="")
+            found = True
+    if not found:
+        print("none")
+    else:
+        print()  
