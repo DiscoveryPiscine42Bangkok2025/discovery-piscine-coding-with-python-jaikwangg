021b7ddf7da96564436b1701490d774a1bf5aec7
jaikwang <kanna9764@gmail.com>
2025-09-16 22:19:23 +0700

    c5 ex10

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    param = sys.argv[1]
+    word = input("What was the parameter? ")
+    if word == param:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
